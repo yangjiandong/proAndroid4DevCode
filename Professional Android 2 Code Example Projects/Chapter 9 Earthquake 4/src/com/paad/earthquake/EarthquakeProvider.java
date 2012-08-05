ea4ba5c23da422c0db3b624c69b1af67873bd7ae
yangjiandong <young.jiandong@gmail.com>
2012-08-05 17:56:45 +0800

    2and,9...

--- Professional Android 2 Code Example Projects/Chapter 9 Earthquake 4/src/com/paad/earthquake/EarthquakeProvider.java
+++ Professional Android 2 Code Example Projects/Chapter 9 Earthquake 4/src/com/paad/earthquake/EarthquakeProvider.java
@@ -14,31 +14,31 @@ import android.util.Log;
 public class EarthquakeProvider extends ContentProvider {
 
   public static final Uri CONTENT_URI = Uri.parse("content://com.paad.provider.earthquake/earthquakes");
-  
+
   @Override
   public boolean onCreate() {
     Context context = getContext();
 
-    earthquakeDatabaseHelper dbHelper = new earthquakeDatabaseHelper(context,  
-                                                                   DATABASE_NAME, 
-                                                                   null, 
+    earthquakeDatabaseHelper dbHelper = new earthquakeDatabaseHelper(context,
+                                                                   DATABASE_NAME,
+                                                                   null,
                                                                    DATABASE_VERSION);
     earthquakeDB = dbHelper.getWritableDatabase();
     return (earthquakeDB == null) ? false : true;
   }
-    
+
   @Override
-  public Cursor query(Uri uri, 
-                      String[] projection, 
-                      String selection, 
-                      String[] selectionArgs, 
+  public Cursor query(Uri uri,
+                      String[] projection,
+                      String selection,
+                      String[] selectionArgs,
                       String sort) {
-        
+
     SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
     qb.setTables(EARTHQUAKE_TABLE);
 
-    // If this is a row query, limit the result set to the passed in row. 
+    // If this is a row query, limit the result set to the passed in row.
     switch (uriMatcher.match(uri)) {
       case QUAKE_ID: qb.appendWhere(KEY_ID + "=" + uri.getPathSegments().get(1));
                      break;
@@ -54,26 +54,26 @@ public class EarthquakeProvider extends ContentProvider {
     }
 
     // Apply the query to the underlying database.
-    Cursor c = qb.query(earthquakeDB, 
-                        projection, 
-                        selection, selectionArgs, 
-                        null, null, 
+    Cursor c = qb.query(earthquakeDB,
+                        projection,
+                        selection, selectionArgs,
+                        null, null,
                         orderBy);
 
     // Register the contexts ContentResolver to be notified if
-    // the cursor result set changes. 
+    // the cursor result set changes.
     c.setNotificationUri(getContext().getContentResolver(), uri);
-    
+
     // Return a cursor to the query result.
     return c;
   }
 
   @Override
   public Uri insert(Uri _uri, ContentValues _initialValues) {
-    // Insert the new row, will return the row number if 
+    // Insert the new row, will return the row number if
     // successful.
     long rowID = earthquakeDB.insert(EARTHQUAKE_TABLE, "quake", _initialValues);
-          
+
     // Return a URI to the newly inserted row on success.
     if (rowID > 0) {
       Uri uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
@@ -86,7 +86,7 @@ public class EarthquakeProvider extends ContentProvider {
   @Override
   public int delete(Uri uri, String where, String[] whereArgs) {
     int count;
-    
+
     switch (uriMatcher.match(uri)) {
       case QUAKES:
         count = earthquakeDB.delete(EARTHQUAKE_TABLE, where, whereArgs);
@@ -96,7 +96,7 @@ public class EarthquakeProvider extends ContentProvider {
         String segment = uri.getPathSegments().get(1);
         count = earthquakeDB.delete(EARTHQUAKE_TABLE, KEY_ID + "="
                                     + segment
-                                    + (!TextUtils.isEmpty(where) ? " AND (" 
+                                    + (!TextUtils.isEmpty(where) ? " AND ("
                                     + where + ')' : ""), whereArgs);
         break;
 
@@ -111,14 +111,14 @@ public class EarthquakeProvider extends ContentProvider {
   public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
     int count;
     switch (uriMatcher.match(uri)) {
-      case QUAKES: count = earthquakeDB.update(EARTHQUAKE_TABLE, values, 
+      case QUAKES: count = earthquakeDB.update(EARTHQUAKE_TABLE, values,
                                                where, whereArgs);
                    break;
 
       case QUAKE_ID: String segment = uri.getPathSegments().get(1);
-                     count = earthquakeDB.update(EARTHQUAKE_TABLE, values, KEY_ID 
-                             + "=" + segment 
-                             + (!TextUtils.isEmpty(where) ? " AND (" 
+                     count = earthquakeDB.update(EARTHQUAKE_TABLE, values, KEY_ID
+                             + "=" + segment
+                             + (!TextUtils.isEmpty(where) ? " AND ("
                              + where + ')' : ""), whereArgs);
                      break;
 
@@ -128,7 +128,7 @@ public class EarthquakeProvider extends ContentProvider {
     getContext().getContentResolver().notifyChange(uri, null);
     return count;
   }
-  
+
   @Override
   public String getType(Uri uri) {
     switch (uriMatcher.match(uri)) {
@@ -137,8 +137,8 @@ public class EarthquakeProvider extends ContentProvider {
       default: throw new IllegalArgumentException("Unsupported URI: " + uri);
     }
   }
-  
-  // Create the constants used to differentiate between the different URI 
+
+  // Create the constants used to differentiate between the different URI
   // requests.
   private static final int QUAKES = 1;
   private static final int QUAKE_ID = 2;
@@ -146,14 +146,14 @@ public class EarthquakeProvider extends ContentProvider {
   private static final UriMatcher uriMatcher;
 
   // Allocate the UriMatcher object, where a URI ending in 'earthquakes' will
-  // correspond to a request for all earthquakes, and 'earthquakes' with a 
+  // correspond to a request for all earthquakes, and 'earthquakes' with a
   // trailing '/[rowID]' will represent a single earthquake row.
   static {
    uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
    uriMatcher.addURI("com.paad.provider.Earthquake", "earthquakes", QUAKES);
    uriMatcher.addURI("com.paad.provider.Earthquake", "earthquakes/#", QUAKE_ID);
   }
-  
+
   //The underlying database
   private SQLiteDatabase earthquakeDB;
 
@@ -178,19 +178,19 @@ public class EarthquakeProvider extends ContentProvider {
   public static final int LATITUDE_COLUMN = 4;
   public static final int MAGNITUDE_COLUMN = 5;
   public static final int LINK_COLUMN = 6;
-      
+
   // Helper class for opening, creating, and managing database version control
   private static class earthquakeDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_CREATE =
-      "create table " + EARTHQUAKE_TABLE + " (" 
+      "create table " + EARTHQUAKE_TABLE + " ("
       + KEY_ID + " integer primary key autoincrement, "
       + KEY_DATE + " INTEGER, "
       + KEY_DETAILS + " TEXT, "
       + KEY_LOCATION_LAT + " FLOAT, "
       + KEY_LOCATION_LNG + " FLOAT, "
-      + KEY_MAGNITUDE + " FLOAT), "
+      + KEY_MAGNITUDE + " FLOAT, "
       + KEY_LINK + " TEXT);";
-        
+
     public earthquakeDatabaseHelper(Context context, String name,
                                     CursorFactory factory, int version) {
       super(context, name, factory, version);
@@ -198,14 +198,14 @@ public class EarthquakeProvider extends ContentProvider {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-      db.execSQL(DATABASE_CREATE);           
+      db.execSQL(DATABASE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
       Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
                   + newVersion + ", which will destroy all old data");
-              
+
       db.execSQL("DROP TABLE IF EXISTS " + EARTHQUAKE_TABLE);
       onCreate(db);
     }
